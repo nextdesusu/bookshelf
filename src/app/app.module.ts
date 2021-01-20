089ef6ecccc1970c921177709a6486d088fce137
nextdesusu <nextdesusu@gmail.com>
2021-01-20 16:12:52 +0400

    feat: generate main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShelfComponent } from './shelf/shelf.component';
+import { BookComponent } from './book/book.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShelfComponent,
+    BookComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule
